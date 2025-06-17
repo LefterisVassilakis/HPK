4bd430ac1e3382fe07e2274df645869c5c8998e8
LefterisVassilakis <vassilakis@ics.forth.gr>
2025-06-17 08:36:51 +0000

    Fix hostname length limit

--- compute/podhandler/templates.go
+++ compute/podhandler/templates.go
@@ -30,6 +30,7 @@ import (
 
 var genericMap = map[string]interface{}{
 	"param": EscapeSingleQuote,
+	"truncate": truncate,
 }
 
 // ParseTemplate returns a custom 'text/template' enhanced with functions for processing HPK templates.
@@ -51,6 +52,13 @@ func EscapeSingleQuote(str ...interface{}) string {
 		}
 	}
 	return strings.Join(out, " ")
+}
+
+func truncate(s string, max int) string {
+    if len(s) <= max {
+        return s
+    }
+    return s[:max]
 }
 
 func strval(v interface{}) string {
@@ -330,10 +338,10 @@ exec {{$.HostEnv.ApptainerBin}} exec --containall --net --fakeroot --scratch /sc
 --apply-cgroups {{.VirtualEnv.CgroupFilePath}} 		\
 {{- end}}
 --env PARENT=${PPID}								\
---bind $HOME/.k8sfs/kubernetes:/k8s-data			\
+--bind $HOME/.hpk-master/kubernetes:/k8s-data			\
 --bind /etc/apptainer/apptainer.conf				\
 --bind $HOME,/tmp									\
---hostname {{.Pod.Name}}							\
+--hostname {{truncate .Pod.Name 63}}							\
 {{$.PauseImageFilePath}} /usr/local/bin/hpk-pause -namespace {{.Pod.Namespace}} -pod {{.Pod.Name}} ||
 echo "[HOST] **SYSTEMERROR** hpk-pause exited with code $?" | tee {{.VirtualEnv.SysErrorFilePath}}
 
